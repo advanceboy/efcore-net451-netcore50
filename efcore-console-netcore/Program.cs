a834b123d719bf6895c7ba893bde5c98421360ff
advanceboy.dolphin <advanceboy.dolphin@gmail.com>
2016-06-18 21:34:45 +0900

    static クラス式の EF ツール対応 DI型 初期化 に変更

--- efcore-console-netcore/Program.cs
+++ efcore-console-netcore/Program.cs
@@ -8,21 +8,15 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 
 namespace efcore_console {
-    public class Program {
-        public static void Main(string[] args) {
-            new Startup().Run();
-        }
-    }
-
     // https://github.com/aspnet/EntityFramework/blob/1.0.0-rc2/src/Microsoft.EntityFrameworkCore.Tools.Core/Design/Internal/StartupInvoker.cs
     // StartupInvoker の仕様をみるに、 $"Startup{environment}", "Startup", "Program", "App" の順番でクラスを探し、
     // 最初に見つかったクラスから、 "ConfigureServices", $"Configure{environment}Services" メソッドを探す。
     // static メソッドなら そのまま呼び出し、 そうでなければ コンストラクタで初期化してから
     // (.net Core の場合は、コンストラクタの引数で IHostingEnvironment が受け取れる)
-    public class Startup {
-        public IConfigurationRoot Configuration { get; }
+    public class Program {
+        public static IConfigurationRoot Configuration { get; }
 
-        public Startup() {
+        static Program() {
             var builder = new ConfigurationBuilder();
             builder
                 .SetBasePath(System.IO.Directory.GetCurrentDirectory())
@@ -34,8 +28,9 @@ namespace efcore_console {
 
             Configuration = builder.Build();
         }
-
-        public void ConfigureServices(IServiceCollection services) {
+        
+        // EF Core Tools から呼び出される
+        public static void ConfigureServices(IServiceCollection services) {
             services
                 .AddLogging()
                 .AddDbContext<BloggingContext>(options => {
@@ -44,17 +39,17 @@ namespace efcore_console {
                 });
         }
 
-        public void Configure(ILoggerFactory loggerFactory) {
+        public static void Configure(ILoggerFactory loggerFactory) {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
         }
 
-        public void Run() {
+        public static void Main(string[] args) {
             var services = new ServiceCollection();
             ConfigureServices(services);
             var serviceProvider = services.BuildServiceProvider();
 
             Configure(serviceProvider.GetService<ILoggerFactory>());
-            
+
 
             using (var db = ActivatorUtilities.CreateInstance<BloggingContext>(serviceProvider)) {
                 db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
